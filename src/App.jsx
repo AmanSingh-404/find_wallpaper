db491a3c0231ddc51a2dc8f7357ae9796d5af7c3
Aman Singh <amanscode2005@gmail.com>
2026-02-03 14:39:25 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-black'>
+      <h1 className='text-white text-2xl font-bold'>Wallpaper</h1>
+    </div>
+  )
+}
+
+export default App
